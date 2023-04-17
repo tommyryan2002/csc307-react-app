e7522e05febd9b6697e7638895455fc96493f086
Tommy Ryan <tommyryan2002@gmail.com>
2023-04-16 17:19:10 -0700

    Added cors

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,10 +1,28 @@
-import React, {useState} from 'react';
 import Table from './Table';
 import Form from './Form';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 
 function MyApp() {
-  const [characters, setCharacters] = useState([]);  
-
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    fetchAll().then( result => {
+       if (result)
+          setCharacters(result);
+     });
+  }, [] ); 
+  
+  async function fetchAll(){
+    try {
+       const response = await axios.get('http://localhost:8000/users');
+       return response.data.users_list;     
+    }
+    catch (error){
+       //We're not handling errors. Just logging into the console.
+       console.log(error); 
+       return false;         
+    }
+ }
   function updateList(person) {
     setCharacters([...characters, person]);
   }
