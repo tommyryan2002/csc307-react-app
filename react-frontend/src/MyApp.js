bdc1b71c30a6cea69742776a1e7d2eb205fd2c14
Tommy Ryan <tommyryan2002@gmail.com>
2023-04-16 17:50:12 -0700

    added linkage

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -23,8 +23,23 @@ function MyApp() {
        return false;         
     }
  }
-  function updateList(person) {
-    setCharacters([...characters, person]);
+
+  async function makePostCall(person){
+    try {
+      const response = await axios.post('http://localhost:8000/users', person);
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  function updateList(person) { 
+    makePostCall(person).then( result => {
+      if (result && result.status === 200)
+        setCharacters([...characters, person] );
+    });
   }
   
   function removeOneCharacter (index) {
